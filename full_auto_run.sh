fca27c891226c1da8315e3f0228615822dd10b88
nhphucqt <hoangphuc.qti@gmail.com>
2026-05-29 16:22:43 +0000

    updated

--- full_auto_run.sh
+++ full_auto_run.sh
@@ -22,9 +22,9 @@ EXP_BASE=(
 )
 CONFIGS=(
   "arguments/endonerf/default.py"
+  "arguments/stereomis/default.py"
+  "arguments/stereomis/default.py"
   "arguments/endonerf/default.py"
-  "arguments/stereomis/default.py"
-  "arguments/stereomis/default.py"
   "arguments/stereomis/default.py"
   "arguments/stereomis/default.py"
 )
@@ -32,17 +32,15 @@ CONFIGS=(
 for i in "${!DATA_PATHS[@]}"; do
   data_path="${DATA_PATHS[$i]}"
   exp_base="${EXP_BASE[$i]}"
-  data_name="${DATA_NAMES[$i]}"
   config="${CONFIGS[$i]}"
 
-  echo "===== Processing dataset: $data_name ====="
+  echo "===== Processing dataset: $data_path ====="
   DATA_PATH="$DATA_ROOT/$data_path"
   EXP_BASE="$exp_base"
-  DATA_NAME="$data_name"
   CONFIG="$config"
 
-  OUT_DIR="${OUT_ROOT}/mean_eval_${EXP_NAME}"
-  TARGET_DIR="${OUT_ROOT}/${EXP_NAME}"
+  OUT_DIR="${OUT_ROOT}/mean_eval_${EXP_BASE}"
+  TARGET_DIR="${OUT_ROOT}/${EXP_BASE}"
 
   if [ -n "$TARGET_DIR" ] && [ "$TARGET_DIR" != "/" ]; then
       rm -rf "$TARGET_DIR"
@@ -50,21 +48,19 @@ for i in "${!DATA_PATHS[@]}"; do
 
   mkdir -p "$OUT_DIR"
 
-  for i in $(seq -w $start_run 1); do
+  for i in $(seq -w 1 1); do
 
     RUN_OUT_DIR="${OUT_DIR}/run${i}"
     mkdir -p "$RUN_OUT_DIR"
 
-    echo "===== Run $i: $EXP_NAME ====="
+    echo "===== Run $i: $EXP_BASE ====="
 
-    python train.py -s "$DATA_PATH" --expname "$EXP_NAME" --configs "$CONFIG"
-    python render.py --model_path "output/$EXP_NAME" --skip_train --skip_video --configs "$CONFIG"
-    python metrics.py --model_path "output/$EXP_NAME" -p test
+    python train.py -s "$DATA_PATH" --expname "$EXP_BASE" --configs "$CONFIG"
+    python render.py --model_path "output/$EXP_BASE" --skip_train --skip_video --configs "$CONFIG"
+    python metrics.py --model_path "output/$EXP_BASE" -p test
 
-    cp "output/$EXP_NAME/per_view.json" "$RUN_OUT_DIR/per_view.json"
-    cp "output/$EXP_NAME/results.json" "$RUN_OUT_DIR/results.json"
+    cp "output/$EXP_BASE/per_view.json" "$RUN_OUT_DIR/per_view.json"
+    cp "output/$EXP_BASE/results.json" "$RUN_OUT_DIR/results.json"
   done
-
-  start_run=1
 
 done
